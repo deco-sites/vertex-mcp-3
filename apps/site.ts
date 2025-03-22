6946077549eb00eaeac4fffb09ba655c34582cdb
pedrofrxncx <pedro_vaz_franca@hotmail.com>
2025-03-21 22:40:34 -0300

    Update dependencies, refactor dev setup, and remove unused files. Upgraded $fresh to version 1.7.3, updated @deco packages, and added a new helloWorld loader. Removed runtime, tailwind configuration, and various static assets. Refactored main.ts to use Hono framework for handling requests.

--- apps/site.ts
+++ apps/site.ts
@@ -1,30 +1,20 @@
-import { App, AppContext as AC } from "deco/mod.ts";
-import website, { Props } from "apps/website/mod.ts";
-
+import { type App, type AppContext as AC } from "@deco/deco";
 import manifest, { Manifest } from "../manifest.gen.ts";
 
-type WebsiteApp = ReturnType<typeof website>;
-
+// deno-lint-ignore no-empty-interface
+interface Props {}
 /**
  * @title Site
  * @description Start your site from a template or from scratch.
  * @category Tool
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
-export default function Site(
-  state: Props,
-): App<Manifest, Props, [
-  WebsiteApp,
-]> {
+export default function Site(state: Props): App<Manifest, Props> {
   return {
     state,
     manifest,
-    dependencies: [
-      website(state),
-    ],
+    dependencies: [],
   };
 }
-
 export type SiteApp = ReturnType<typeof Site>;
 export type AppContext = AC<SiteApp>;
-export { onBeforeResolveProps, Preview } from "apps/website/mod.ts";
--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
+import { Deco } from "@deco/deco";
+import { mcpServer } from "@deco/mcp";
+import { Hono } from "@hono/hono";
+import manifest, { Manifest } from "./manifest.gen.ts";
 
-import { start } from "$fresh/server.ts";
-import config from "./fresh.config.ts";
-import manifest from "./fresh.gen.ts";
+const app = new Hono();
+const deco = await Deco.init<Manifest>({
+  manifest,
+});
+const envPort = Deno.env.get("PORT");
 
-await start(manifest, config);
+app.use("/*", mcpServer(deco));
+app.all("/*", async (c) => c.res = await deco.fetch(c.req.raw));
+
+Deno.serve({ handler: app.fetch, port: envPort ? +envPort : 8000 });
